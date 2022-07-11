86ded8231fcaa61e64b15d222db87412f1bff6d3
Ihor Mural <igor201278789887000@gmail.com>
2022-07-11 15:58:09 +0300

    Header test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const button = document.querySelector('.header__menu');
+const page = document.querySelector('.page');
+
+button.addEventListener('click', function handleClick() {
+  menu.classList.toggle('menu--active');
+  page.classList.toggle('page--with-menu');
+});
